072268a722a79027becffecf181914ea7cb9a242
Uma Maheswari Thumma <your-email@example.com>
2026-07-31 06:52:00 +0000

    Fix indentation and bind host to 0.0.0.0 for container networking

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-
 metrics.info('app_info', 'Hello World application info', version='1.0.0')
 
 @app.route('/')
